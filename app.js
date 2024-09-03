85fb364a86e7ff736f749ea66cd09aa73c0073ec
Mehar Giri <84210815+mehargiri@users.noreply.github.com>
2024-09-03 16:53:38 -0400

    Added steps for deployment to Render.io using deploy hook in pipeline.yml
    Added a version endpoint in the app.js

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const logger = require('./src/logger.js')
 
-// get the port from env variable
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+
+app.get('/version', (_req, res) => {
+  return res.send('The current version is 1')
+})
 
 app.listen(PORT, () => {
   logger.info(`server started on port ${PORT}`)

8d3e60fe14a5adb619cc3239ff92d2ab06ed6bf7
Mehar Giri <84210815+mehargiri@users.noreply.github.com>
2024-09-09 18:31:35 -0400

    Added discord webhook notifications to pipeline.yml

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (_req, res) => {
-  return res.send('The current version is 4')
+  return res.send('The current version is 5')
 })
 
 app.get('/health', (_req, res) => {
